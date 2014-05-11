79f214fd2d39bb67153ae025cab64d99bab3e234
Snorry0815 <kane_htl@gmx.at>
2014-05-11 12:29:10 +0200

    add unit name inspector

---
+++ AGPW/Assets/AGPW_1_2.1/Scripts/Inspector/UnitIdentificationInspector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AGPW
+{
+	public class UnitIdentificationInspector : MonoBehaviour 
+	{
+		private static string NAME = "UnitIdentificationInspector";
+		private static string TEXT_PREFAB_NAME = "TextPrefab_AGPW_1_2.1";
+
+		private GameObject unitIdentificationText = null;
+
+		void Start () 
+		{
+			Init();
+		}
+
+		private void Init()
+		{
+			if(unitIdentificationText != null)
+			{
+				Destroy(unitIdentificationText);
+			}
+			this.unitIdentificationText = (GameObject)Instantiate(Resources.Load(TEXT_PREFAB_NAME)) as GameObject;
+			this.unitIdentificationText.name = NAME;
+			this.unitIdentificationText.GetComponent<TextMesh>().text = this.gameObject.name;
+
+			this.unitIdentificationText.transform.parent = this.transform;
+		}
+
+		void Update () 
+		{
+			if(this.unitIdentificationText != null)
+			{
+				this.unitIdentificationText.transform.LookAt(2*transform.position - Camera.main.transform.position);
+			}
+		}
+	}
+}
